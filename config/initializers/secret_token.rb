328c55c3e0faaf38a7e0eb6672f31e54fda7edd0
Devin Brown <devinb@ucar.edu>
2013-07-05 11:50:38 -0600

    Add Rails files.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sharefactory::Application.config.secret_token = '1cd798c1c4a0160a5f06adbea669763441656ff03d0f4d57d5e4f16355b6284a170edbf652dcb0d5b9a4e1a494ea95a14b5fb1814e9ba32da02e6f0b8a35fda1'
