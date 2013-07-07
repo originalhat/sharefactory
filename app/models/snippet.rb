0bbcc44512831c0a6a838f38798ab515a9f54e0c
Devin Brown <devin.g.brown@gmail.com>
2013-07-07 15:30:29 -0600

    updating models

--- app/models/snippet.rb
+++ app/models/snippet.rb
@@ -1,4 +1,5 @@
-class Snippets < ActiveRecord::Base
+class Snippet < ActiveRecord::Base
   attr_accessible :content, :downvotes, :upvotes
   belongs_to :user
+  has_many :comments
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :fname, :lname, :password, :username
   has_many :snippets
+  has_many :comments
 end
