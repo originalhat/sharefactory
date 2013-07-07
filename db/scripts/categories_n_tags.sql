cba0ccb7c11533f23721e5e48dafa05127d6b594
Elijah Gartin <elijah.gartin@gmail.com>
2013-07-07 17:57:10 -0600

    Added Mockup Screens in the doc folder and made some changes to the .sql script, disregard at this time though

--- db/scripts/categories_n_tags.sql
+++ db/scripts/categories_n_tags.sql
@@ -5,24 +5,41 @@ categoryname varchar(255) NOT NULL,
 PRIMARY KEY (catID)
 )
 
+;
+
 CREATE TABLE tag
 (
 tagID int NOT NULL AUTO_INCREMENT,
 tagname varchar(255) NOT NULL,
 catID int references category(catID),
 PRIMARY KEY (tagID)
-)
+);
+
+
+
 
 --Need to come up with more categories... the catID is ommitted because it auto increments a value in the primary key field.
-INSERT INTO category (categoryname) VALUES
-	('Front End Development')
+
+INSERT INTO category (categoryname) 
+VALUES
+	('Front End Development'),
+
 	('Back End Development');
+
+
 
 -- Subcategories??? Hopefully not
 		
+
 --I have to wait to insert the category table to be able to give each tag a catID.
+
 -- Might actually split the script into two scripts.
 
---INSERT INTO tag (tagID, tagname, catID) VALUES
+
+
+--INSERT INTO tag (tagID, tagname, catID) 
+--VALUES
+
 --    ('HTML', 1),
+
 --    ('CSS', 1);
