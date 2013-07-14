75da63d5c88a48d7eccff715a869ec079047c358
Devin Brown <devin.g.brown@gmail.com>
2013-07-14 15:44:18 -0600

    devise / users / routes TLC

--- db/migrate/20130707211241_create_users.rb
+++ db/migrate/20130707211241_create_users.rb
@@ -1,11 +1,9 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :username, :null => false
-      t.string :email, :null => false
-      t.string :password, :null => false
-      t.string :fname, :null => false
-      t.string :lname, :null => false
+      t.string :username
+      t.string :fname
+      t.string :lname
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130707213239) do
+ActiveRecord::Schema.define(:version => 20130712141822) do
+
+  create_table "categories", :force => true do |t|
+    t.string   "name",       :null => false
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "comments", :force => true do |t|
     t.text     "content",    :null => false
@@ -37,13 +43,24 @@ ActiveRecord::Schema.define(:version => 20130707213239) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "username",   :null => false
-    t.string   "email",      :null => false
-    t.string   "password",   :null => false
-    t.string   "fname",      :null => false
-    t.string   "lname",      :null => false
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.string   "username"
+    t.string   "fname"
+    t.string   "lname"
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
+    t.string   "email",                  :default => "", :null => false
+    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
