380ba5b732cd1f52333f9a0ce6b50c86feb8c4cd
Devin Brown <devin.g.brown@gmail.com>
2013-07-16 20:45:42 -0600

    include link / route to settings page

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,11 @@ Sharefactory::Application.routes.draw do
   get "/user", to: "users#index"
   get "/snippets", to: "snippets#index"
 
+  # override devise routes
   devise_scope :user do
     get "/login" => "devise/sessions#new"
-    get "/register", to: "devise/registrations#new"
     get "/logout" => "devise/sessions#destroy"
+    get "/register", to: "devise/registrations#new"
+    get "/settings", to: "devise/registrations#edit"
   end
 end
